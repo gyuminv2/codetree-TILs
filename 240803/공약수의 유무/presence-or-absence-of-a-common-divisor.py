0b7831dd7a419744569c29e68a89b1720d451610
gyumin.park <48640323+gyuminv2@users.noreply.github.com>
2024-08-03 15:24:47 +0900

    Title: 공약수의 유무, Time: 114ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240803/공약수의 유무/presence-or-absence-of-a-common-divisor.py
@@ -0,0 +1,13 @@
+a, b = map(int, input().split())
+cd = []
+for i in range(1, 1920):
+    if 1920 % i == 0:
+        cd.append(i)
+for i in range(1, 2880):
+    if 2880 % i == 0:
+        cd.append(i)
+for i in range(a, b+1):
+    if i in cd:
+        print(1)
+        exit()
+print(0)
