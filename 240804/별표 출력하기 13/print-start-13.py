4085512ea91a1527400807927c5738d0059fdd10
gyumin.park <48640323+gyuminv2@users.noreply.github.com>
2024-08-04 15:06:05 +0900

    Title: 별표 출력하기 13, Time: 116ms, Memory: 24MB, Status: Passed - CodeTree

--- 240804/별표 출력하기 13/print-start-13.py
+++ 240804/별표 출력하기 13/print-start-13.py
@@ -13,13 +13,25 @@ for i in range(1, n+1):
             print('*', '', end='')
         b += 1
     print()
-for i in range(1, n+1):
-    if i % 2 == 0:
-        for j in range(a):
-            print('*', '', end='')
-        a -= 1
-    else:
-        for j in range(b):
-            print('*', '', end='')
-        b += 1
-    print()
+if n % 2 != 0:
+    for i in range(1, n+1):
+        if i % 2 == 0:
+            for j in range(a):
+                print('*', '', end='')
+            a -= 1
+        else:
+            for j in range(b):
+                print('*', '', end='')
+            b += 1
+        print()
+else:
+    for i in range(1, n+1):
+        if i % 2 != 0:
+            for j in range(a):
+                print('*', '', end='')
+            a -= 1
+        else:
+            for j in range(b):
+                print('*', '', end='')
+            b += 1
+        print()
