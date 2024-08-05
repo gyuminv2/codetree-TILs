136e67778a653d48511e187f1dc2db9171532572
gyumin.park <48640323+gyuminv2@users.noreply.github.com>
2024-08-05 18:39:57 +0900

    Title: 특정 문자로 시작하는 문자열, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240805/특정 문자로 시작하는 문자열/strings-that-start-with-a-specific-character.py
@@ -0,0 +1,12 @@
+n = int(input())
+arr = []
+for _ in range(n):
+    arr.append(input())
+c = input()
+rtn = 0
+for i in range(n):
+    if arr[i][0] != c:
+        arr.pop(i)
+    else:
+        rtn += len(arr[i])
+print(f'{len(arr)} {rtn/len(arr):.2f}')
