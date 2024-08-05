6d873c4248daa6e01ab5fc1e7578d21afe78c21a
gyumin.park <48640323+gyuminv2@users.noreply.github.com>
2024-08-05 12:05:56 +0900

    Title: 격자 모양 출력하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240805/격자 모양 출력하기/print-grid-shape.py
@@ -0,0 +1,12 @@
+n, m = map(int, input().split())
+
+arr = [[0 for _ in range(n)] for _ in range(m)]
+
+for i in range(n):
+    k, l = map(int, input().split())
+    arr[k-1][l-1] = k*l
+
+for i in range(n):
+    for j in range(m):
+        print(arr[i][j], end=' ')
+    print()
