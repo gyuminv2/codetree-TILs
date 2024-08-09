1ffbec3e52c142b610809e303d460807cddb4e8b
gyumin.park <48640323+gyuminv2@users.noreply.github.com>
2024-08-09 15:16:22 +0900

    Title: 작은 구슬의 이동, Time: 143ms, Memory: 24MB, Status: Passed - CodeTree

--- 240809/작은 구슬의 이동/small-marble-movement.py
+++ 240809/작은 구슬의 이동/small-marble-movement.py
@@ -15,19 +15,21 @@ mapper = {
 }
 
 def in_range(x):
-    return 0 <= x and x < n
+    return 0 < x and x <= n
 
 move_dir = mapper[d]
-for i in range(t+1):
+for i in range(t):
     if move_dir == mapper['R'] or move_dir == mapper['L']:
         nx = c + dxs[move_dir]
         if not in_range(nx):
             move_dir = 3 - move_dir
+            continue
         c += dxs[move_dir]
     else:
         ny = r + dys[move_dir]
         if not in_range(ny):
             move_dir = 3 - move_dir
+            continue
         r += dys[move_dir]
 
 print(r, c)
