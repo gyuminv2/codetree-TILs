98bdf2deb085f3b016ee3f4efcc244726d38892f
gyumin.park <48640323+gyuminv2@users.noreply.github.com>
2024-08-10 14:44:27 +0900

    Title: 컨베이어 벨트, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240810/컨베이어 벨트/conveyor-belt.py
@@ -0,0 +1,20 @@
+n, t = map(int, input().split())
+
+arr = []
+for _ in range(2):
+    arr.append(list(map(int, input().split())))
+
+for i in range(t):
+    tmpmp = arr[1][2]
+    for j in range(1, -1, -1):
+        tmp = arr[j][2]
+        for k in range(n-2, -1, -1):
+            arr[j][k+1] = arr[j][k]
+        if j != 1:
+            arr[j+1][0] = tmp
+    arr[0][0] = tmpmp
+
+for i in range(2):
+    for j in range(n):
+        print(arr[i][j], end=' ')
+    print()
