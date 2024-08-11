d724768b40f6821156810cc72c620fbd68a9d9bc
gyumin.park <48640323+gyuminv2@users.noreply.github.com>
2024-08-11 23:17:53 +0900

    Title: 그 계절, 그 날, Time: 103ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240811/그 계절, 그 날/that-season-that-day.py
@@ -0,0 +1,35 @@
+Y, M, D = map(int, input().split())
+
+def check_yoon(Y):
+    if (Y % 4 == 0 and Y % 100 == 0) or (Y % 400):
+        return 1
+    return 0
+
+yoon = check_yoon(Y)
+
+def check_season(M, D):
+    if 3 <= M and M <= 5:
+        if 4 == M and D == 31:
+            return -1
+        return 'Spring'
+    if 6 <= M and M <= 8:
+        if 6 == M and D == 31:
+            return -1
+        return 'Summer'
+    if 9 <= M and M <= 11:
+        if (9 == M or 11 == M) and D == 31:
+            return -1
+        return 'Fall'
+    if 12 == M or 1 == M:
+        if 1 == M and D == 31:
+            return -1
+        return 'Winter'
+    if 2 == M:
+        if yoon == 1 and (D == 30 or D == 31):
+            return -1
+        else:
+            return 'Winter'
+        
+    
+season = check_season(M, D)
+print(season)
