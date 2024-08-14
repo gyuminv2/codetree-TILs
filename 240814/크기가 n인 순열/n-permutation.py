248b88750887193ec548e5718f7f59c128924cb1
gyumin.park <48640323+gyuminv2@users.noreply.github.com>
2024-08-14 19:00:24 +0900

    Title: 크기가 n인 순열, Time: 336ms, Memory: 29MB, Status: Passed - CodeTree

---
+++ 240814/크기가 n인 순열/n-permutation.py
@@ -0,0 +1,22 @@
+n = int(input())
+visited = [0 for _ in range(n+1)]
+ans = []
+
+def choose(c):
+    if c == n+1:
+        print(*ans)
+        return
+
+    for i in range(1, n+1):
+        if visited[i] == True:
+            continue
+
+        visited[i] = True
+        ans.append(i)
+
+        choose(c + 1)
+
+        ans.pop()
+        visited[i] = False
+
+choose(1)
