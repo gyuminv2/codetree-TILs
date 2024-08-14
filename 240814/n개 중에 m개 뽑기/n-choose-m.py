a97e9bd2e83f8f4c901ae6e9a5fbb84b8e49d003
gyumin.park <48640323+gyuminv2@users.noreply.github.com>
2024-08-14 18:22:08 +0900

    Title: n개 중에 m개 뽑기, Time: 145ms, Memory: 25MB, Status: Passed - CodeTree

--- 240814/n개 중에 m개 뽑기/n-choose-m.py
+++ 240814/n개 중에 m개 뽑기/n-choose-m.py
@@ -1,22 +1,24 @@
 k, n = map(int, input().split())
 ans = []
 
-# def sort(ans, n):
-#     for i in range(n-1):
-#         if ans[i] >= ans[i+1]:
-#             return False
-#     return True
-
 def sort(ans, n):
-    for i in range(n-1, 0, -1):
-        if ans[i-1] >= ans[i]:
+    for i in range(n-1):
+        if ans[i] >= ans[i+1]:
             return False
-    return True    
+    return True
+
+def check_last(ans, j):
+    if len(ans) >= 1:
+            if j <= ans[len(ans)-1]:
+                return True
+    return False
 
 def rc(i):
     if i == 0:
         return
     for j in range(1, k+1):
+        if check_last(ans, j) == True:
+            continue
         ans.append(j)
         if len(ans) == n:
             if sort(ans, n) == True:
