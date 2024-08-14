22b83e757557f1f80b5a967f493bb9faa4bc70cf
gyumin.park <48640323+gyuminv2@users.noreply.github.com>
2024-08-14 15:22:15 +0900

    Title: k개 중에 1개를 n번 뽑기, Time: 117ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240814/k개 중에 1개를 n번 뽑기/n-permutations-of-k-with-repetition.py
+++ 240814/k개 중에 1개를 n번 뽑기/n-permutations-of-k-with-repetition.py
@@ -2,19 +2,15 @@ k, n = map(int, input().split())
 
 ans = []
 
+f = k
 def gang(k, n):
     if k == 0:
         return
-    for i in range(1, n+1):
-        ans.append(i)
-        gang(k-1, n)
-        if len(ans) == 3:
+    for i in range(f):
+        ans.append(i+1)
+        if len(ans) == n:
             print(*ans)
+        gang(k-1, n)
         ans.pop()
 
-def __for(ans):
-    for i in range(len(ans)):
-        print(ans[i])
-
 gang(k, n)
-__for(ans)
