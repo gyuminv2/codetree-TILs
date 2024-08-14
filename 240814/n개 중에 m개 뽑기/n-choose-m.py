50734be28a153b875388b43de3b0ef9cb01edeb0
gyumin.park <48640323+gyuminv2@users.noreply.github.com>
2024-08-14 18:02:02 +0900

    Title: n개 중에 m개 뽑기, Time: 5090ms, Memory: 28MB, Status: Time Limit Exceed - CodeTree

--- 240814/n개 중에 m개 뽑기/n-choose-m.py
+++ 240814/n개 중에 m개 뽑기/n-choose-m.py
@@ -1,18 +1,17 @@
 k, n = map(int, input().split())
 ans = []
-tmp = ()
 
-def re(ans, n):
-    for i in range(n-2):
-        if ans[i] != ans[i+1] and ans[i+1] != ans[i+2]:
-            return True
-    return False
+# def sort(ans, n):
+#     for i in range(n-1):
+#         if ans[i] >= ans[i+1]:
+#             return False
+#     return True
 
 def sort(ans, n):
-    for i in range(n-1):
-        if ans[i] >= ans[i+1]:
+    for i in range(n-1, 0, -1):
+        if ans[i-1] >= ans[i]:
             return False
-    return True
+    return True    
 
 def rc(i):
     if i == 0:
