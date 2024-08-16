5dab4a606b034697a7eab6cda6a312413a66e104
gyumin.park <48640323+gyuminv2@users.noreply.github.com>
2024-08-16 20:10:01 +0900

    Title: 나이트, Time: 163ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240816/나이트/knight-movements.py
@@ -0,0 +1,48 @@
+from collections import deque
+
+n = int(input())
+
+x1, y1, x2, y2 = list(map(int, input().split()))
+x1 -= 1
+y1 -= 1
+x2 -= 1
+y2 -= 1
+
+visited = [[0 for _ in range(n)] for _ in range(n)]
+step = [[0 for _ in range(n)] for _ in range(n)]
+
+q = deque()
+
+def in_range(x, y):
+    return 0 <= x and x < n and 0 <= y and y < n
+
+def can_go(x, y):
+    if not in_range(x, y):
+        return False
+    if visited[x][y] == 1:
+        return False
+    return True
+
+def push(x, y, s):
+    step[x][y] = s
+    visited[x][y] = 1
+    q.append((x, y))
+
+def bfs():
+    dxs = [-1, -2, -2, -1, 1, 2, 2, 1]
+    dys = [-2, -1, 1, 2, 2, 1, -1, -2]
+
+    while q:
+        x, y = q.popleft()
+
+        for dx, dy in zip(dxs, dys):
+            nx, ny = x + dx, y + dy
+            if can_go(nx, ny):
+                push(nx, ny, step[x][y] + 1)
+
+push(x1, y1, step[x1][y1])
+bfs()
+if step[x2][y2] == 0:
+    print(-1)
+else:
+    print(step[x2][y2])
