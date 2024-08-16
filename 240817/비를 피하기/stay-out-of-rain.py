5623b7169e22415ba94b67a5e7c6b6f9a83fe533
gyumin.park <48640323+gyuminv2@users.noreply.github.com>
2024-08-17 05:05:55 +0900

    Title: 비를 피하기, Time: 501ms, Memory: 30MB, Status: Wrong Answer - CodeTree

---
+++ 240817/비를 피하기/stay-out-of-rain.py
@@ -0,0 +1,76 @@
+from collections import deque
+
+n, h, m = map(int, input().split())
+
+grid = []
+for _ in range(n):
+    grid.append(list(map(int, input().split())))
+
+human_x = []
+human_y = []
+point_x = []
+point_y = []
+for x in range(n):
+    for y in range(n):
+        if grid[x][y] == 2:
+            human_x.append(x)
+            human_y.append(y)
+        if grid[x][y] == 3:
+            point_x.append(x)
+            point_y.append(y)
+
+q = deque()
+
+def in_range(x, y):
+    return 0 <= x and x < n and 0 <= y and y < n
+
+def can_go(x, y):
+    if not in_range(x, y):
+        return False
+    if visited[x][y] == 1 or grid[x][y] == 1:
+        return False
+    return True
+
+def push(x, y, s):
+    visited[x][y] = 1
+    step[x][y] = s
+    q.append((x, y))
+
+def bfs(end_x, end_y):
+    rtn = 0
+
+    dxs = [-1, 1, 0, 0]
+    dys = [0, 0, -1, 1]
+
+    while q:
+        x, y = q.popleft()
+        for dx, dy in zip(dxs, dys):
+            nx, ny = x + dx, y + dy
+            if can_go(nx, ny):
+                push(nx, ny, step[x][y] + 1)
+                if nx == end_x and ny == end_y:
+                    rtn = 1
+
+ans = [[99999 for _ in range(n)] for _ in range(n)]
+
+for s_x, s_y in zip(human_x, human_y):
+    for e_x, e_y in zip(point_x, point_y):
+        visited = [[0 for _ in range(n)] for _ in range(n)]
+        step = [[0 for _ in range(n)] for _ in range(n)]
+
+        push(s_x, s_y, step[s_x][s_y])
+        bfs(e_x, e_y)
+        time = step[e_x][e_y]
+        if ans[s_x][s_y] > time:
+            ans[s_x][s_y] = time
+        elif time == 0:
+            ans[s_x][s_y] = -1
+
+for x in range(n):
+    for y in range(n):
+        if ans[x][y] == 99999:
+            print(0, end=' ')
+        else:
+            print(ans[x][y], end=' ')
+
+    print('')
