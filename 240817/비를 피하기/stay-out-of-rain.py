a67b4ca05a91a9ae11a241ca505f68b7c76fbe99
gyumin.park <48640323+gyuminv2@users.noreply.github.com>
2024-08-17 05:42:33 +0900

    Title: 비를 피하기, Time: 5034ms, Memory: 30MB, Status: Time Limit Exceed - CodeTree

--- 240817/비를 피하기/stay-out-of-rain.py
+++ 240817/비를 피하기/stay-out-of-rain.py
@@ -33,12 +33,11 @@ def can_go(x, y):
 
 def push(x, y, s):
     visited[x][y] = 1
-    step[x][y] = s
+    if step[x][y] < s:
+        step[x][y] = s
     q.append((x, y))
 
 def bfs(end_x, end_y):
-    rtn = 0
-
     dxs = [-1, 1, 0, 0]
     dys = [0, 0, -1, 1]
 
@@ -48,11 +47,8 @@ def bfs(end_x, end_y):
             nx, ny = x + dx, y + dy
             if can_go(nx, ny):
                 push(nx, ny, step[x][y] + 1)
-                if nx == end_x and ny == end_y:
-                    rtn = 1
 
 ans = [[0 for _ in range(n)] for _ in range(n)]
-
 for s_x, s_y in zip(human_x, human_y):
     for e_x, e_y in zip(point_x, point_y):
         visited = [[0 for _ in range(n)] for _ in range(n)]
@@ -61,17 +57,20 @@ for s_x, s_y in zip(human_x, human_y):
         push(s_x, s_y, step[s_x][s_y])
         bfs(e_x, e_y)
         time = step[e_x][e_y]
+        if time == 0:
+            continue
+
         if ans[s_x][s_y] != 0:
             tmp = ans[s_x][s_y]
             if tmp > time:
                 ans[s_x][s_y] = time
         else:
             ans[s_x][s_y] = time
-        if time == 0:
-            ans[s_x][s_y] = -1
+
+    if ans[s_x][s_y] == 0:
+        ans[s_x][s_y] = -1
 
 for x in range(n):
     for y in range(n):
         print(ans[x][y], end=' ')
-
     print('')
