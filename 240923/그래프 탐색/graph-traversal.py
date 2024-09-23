35b714eea57284099539d99a5e5ee943c76db6fd
gyumin.park <48640323+gyuminv2@users.noreply.github.com>
2024-09-23 13:55:57 +0900

    Title: 그래프 탐색, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240923/그래프 탐색/graph-traversal.py
@@ -0,0 +1,24 @@
+n, m = map(int, input().split())
+
+graph = [[] for _ in range(n+1)]
+visited = [False] * (n+1)
+
+cnt = 0
+
+def dfs(v):
+    global cnt
+
+    for i in graph[v]:
+        if not visited[i]:
+            cnt += 1
+            dfs(i)
+
+for _ in range(m):
+    s, e = map(int, input().split())
+    graph[s].append(e)
+    graph[e].append(s)
+
+visited[v] = True
+dfs(1)
+
+print(cnt)
