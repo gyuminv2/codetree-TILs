9aa4647637bbbc258e1cbbc63973be32f40077b4
gyumin.park <48640323+gyuminv2@users.noreply.github.com>
2024-11-06 15:14:44 +0900

    Title: 중앙값 계산 2, Time: 45ms, Memory: 16MB, Status: Passed - CodeTree

--- 241106/중앙값 계산 2/get-median-2.py
+++ 241106/중앙값 계산 2/get-median-2.py
@@ -1,8 +1,11 @@
 n = int(input())
 arr = list(map(int, input().split()))
+tmp = []
 mid = []
 
 for i in range(n):
+    tmp.append(arr[i])
     if i % 2 == 0:
-        mid.append(arr[i//2])
+        tmp.sort()
+        mid.append(tmp[i//2])
 print(*mid)
