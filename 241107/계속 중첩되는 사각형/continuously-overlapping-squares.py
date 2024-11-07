3655cc1374dfdc62febd7901feec57a9ee42301b
gyumin.park <48640323+gyuminv2@users.noreply.github.com>
2024-11-07 12:28:46 +0900

    Title: 계속 중첩되는 사각형, Time: 48ms, Memory: 15MB, Status: Wrong Answer - CodeTree

---
+++ 241107/계속 중첩되는 사각형/continuously-overlapping-squares.py
@@ -0,0 +1,44 @@
+n = int(input())
+
+arr = []
+for _ in range(n):
+    arr.append(list(map(int, input().split())))
+
+def diff(arr, i):
+    a = arr[i][0]
+    b = arr[i][2]
+    c = arr[i][1]
+    d = arr[i][3]
+    
+    if a <= arr[i-1][0]:
+        a = arr[i-1][0]
+    elif a >= arr[i-1][2]:
+        a = arr[i-1][2]
+    
+    if b <= arr[i-1][0]:
+        b = arr[i-1][0]
+    elif b >= arr[i-1][2]:
+        b = arr[i-1][2]
+
+    if c <= arr[i-1][1]:
+        c = arr[i-1][1]
+    elif c >= arr[i-1][3]:
+        c = arr[i-1][3]
+
+    if d <= arr[i-1][1]:
+        d = arr[i-1][1]
+    elif d >= arr[i-1][3]:
+        d = arr[i-1][3]
+
+    return((b-a)*(d-c))
+
+blue = 0
+for i in range(len(arr)):
+    if i == 0 :
+        continue
+    if i % 2 == 0:
+        blue -= diff(arr, i)
+    else:
+        blue += (arr[i][2] - arr[i][0]) * (arr[i][3] - arr[i][1])
+
+print(blue)
