8a5e69b6af2bb13fa8df085ee1430d2c2cb94996
gyumin.park <48640323+gyuminv2@users.noreply.github.com>
2024-11-07 16:46:30 +0900

    Title: T를 초과하는 연속 부분 수열, Time: 41ms, Memory: 15MB, Status: Passed - CodeTree

--- 241107/T를 초과하는 연속 부분 수열/subsequence-above-t.py
+++ 241107/T를 초과하는 연속 부분 수열/subsequence-above-t.py
@@ -5,15 +5,15 @@ arr.append(list(map(int, input().split())))
 
 ret = []
 cnt = 0
-for i in range(n-1):
-    nxt = arr[0][i+1]
-    if i == 0:
-        continue
-    if arr[0][i]+1 == nxt:
+for i in range(n):
+    if arr[0][i] > t:
         cnt += 1
     else:
         ret.append(cnt)
         cnt = 0
     ret.append(cnt)
 
-print(max(ret)+1)
+if not ret:
+    print(1)
+else:
+    print(max(ret))
