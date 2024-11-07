ec321a874697e03332f0c05d8e1ba9ad56dca8ca
gyumin.park <48640323+gyuminv2@users.noreply.github.com>
2024-11-07 12:37:35 +0900

    Title: 계속 중첩되는 사각형, Time: 39ms, Memory: 15MB, Status: Wrong Answer - CodeTree

--- 241107/계속 중첩되는 사각형/continuously-overlapping-squares.py
+++ 241107/계속 중첩되는 사각형/continuously-overlapping-squares.py
@@ -10,27 +10,27 @@ def diff(arr, i):
     c = arr[i][1]
     d = arr[i][3]
     
-    if a <= arr[i-1][0]:
+    if a < arr[i-1][0]:
         a = arr[i-1][0]
-    elif a >= arr[i-1][2]:
+    elif a > arr[i-1][2]:
         a = arr[i-1][2]
     
-    if b <= arr[i-1][0]:
+    if b < arr[i-1][0]:
         b = arr[i-1][0]
-    elif b >= arr[i-1][2]:
+    elif b > arr[i-1][2]:
         b = arr[i-1][2]
 
-    if c <= arr[i-1][1]:
+    if c < arr[i-1][1]:
         c = arr[i-1][1]
-    elif c >= arr[i-1][3]:
+    elif c > arr[i-1][3]:
         c = arr[i-1][3]
 
-    if d <= arr[i-1][1]:
+    if d < arr[i-1][1]:
         d = arr[i-1][1]
-    elif d >= arr[i-1][3]:
+    elif d > arr[i-1][3]:
         d = arr[i-1][3]
 
-    return((b-a)*(d-c))
+    return(abs(b-a)*abs(d-c))
 
 blue = 0
 for i in range(len(arr)):
@@ -39,6 +39,6 @@ for i in range(len(arr)):
     if i % 2 == 0:
         blue -= diff(arr, i)
     else:
-        blue += (arr[i][2] - arr[i][0]) * (arr[i][3] - arr[i][1])
+        blue += abs(arr[i][2] - arr[i][0]) * abs(arr[i][3] - arr[i][1])
 
 print(blue)
