c2aae8939b10b8fc1087dc17ab67ff7684e45af8
gyumin.park <48640323+gyuminv2@users.noreply.github.com>
2024-11-09 03:51:00 +0900

    Title: 이동경로상에 있는 모든 숫자 더하기, Time: 61ms, Memory: 17MB, Status: Wrong Answer - CodeTree

--- 241109/이동경로상에 있는 모든 숫자 더하기/add-all-the-numbers-on-the-path.py
+++ 241109/이동경로상에 있는 모든 숫자 더하기/add-all-the-numbers-on-the-path.py
@@ -4,35 +4,37 @@ grid = []
 for _ in range(n):
     grid.append(list(map(int, input().split())))
 
-# 위 오 아 왼
+# 상, 우, 하, 좌 방향 이동
 dxs = [0, 1, 0, -1]
 dys = [-1, 0, 1, 0]
 
 dirs = [0, 1, 2, 3]
 
+# 중앙 시작
 s = n // 2
 ret = []
 ret.append(grid[s][s])
 
 def in_range(x, y):
-    return 0 <= x and x < n and 0 <= y and y < n
+    return 0 <= x < n and 0 <= y < n
 
 def letsGo():
-    cur = [s, s, 0]
+    cur = [s, s, 0]  # x, y, 방향
     for i in range(t):
         if cmd[i] == 'R':
-            cur[2] = dirs[(cur[2]+1)%4]
+            cur[2] = (cur[2] + 1) % 4  # 오른쪽으로 회전
         elif cmd[i] == 'L':
-            cur[2] = dirs[(cur[2]-1)%4]
-        else :
-            dx = cur[0] + dxs[dirs[cur[2]]]
-            dy = cur[1] + dys[dirs[cur[2]]]
+            cur[2] = (cur[2] - 1) % 4  # 왼쪽으로 회전
+        else:
+            # 전진할 위치 계산
+            dx = cur[0] + dxs[cur[2]]
+            dy = cur[1] + dys[cur[2]]
             if in_range(dx, dy):
                 cur[0] = dx
                 cur[1] = dy
-                if ret[-1] == grid[dy][dx]:
-                    continue
-                else:
+                # 새 위치에 있는 값을 ret에 추가 (마지막 값과 다른 경우에만)
+                if ret[-1] != grid[dy][dx]:  # y, x 순서 주의
                     ret.append(grid[dy][dx])
+
 letsGo()
 print(sum(ret))
