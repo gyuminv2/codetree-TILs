992a9bffbd64f477b74f2c347727058efd5a4627
gyumin.park <48640323+gyuminv2@users.noreply.github.com>
2025-04-12 00:59:09 +0900

    Title: 코드트리 빵, Time: 1073ms, Memory: 20MB, Status: Time Limit Exceed - Codetree

---
+++ 250412/코드트리 빵/codetree-mon-bread.py
@@ -0,0 +1,164 @@
+# move_man, TARGET : CU
+def move_man(idx):
+    mi, mj = find_man(idx)
+    ci, cj = find_cu(-idx)
+    ri, rj = mi, mj
+    flg = 0
+    # man의 초기 이동일 경우 -> (현재 위치 값 : idx => 999로 변경 (원래 베이스캠프 위치로..))
+    if [mi, mj] in base_camp:
+        flg = 1
+        base_camp.pop(base_camp.index([mi, mj]))
+    
+    mn_dist = abs(mi-ci) + abs(mj-cj)
+    for dr in [0, 1, 2, 3]:
+        ni, nj = mi + dis[dr], mj + djs[dr]
+        dist = abs(ni-ci) + abs(nj-cj)
+        # 격자 체크 (0보다 크면 안됨)
+        if 0<=ni<n and 0<=nj<n and grid[ni][nj] <= 0:
+            # 편의점 도착
+            if (ni, nj) == (ci, cj):
+                if flg == 1:
+                    grid[mi][mj] = 999
+                else:
+                    grid[mi][mj] = 0
+                grid[ni][nj] = idx
+                return 1
+            if mn_dist > dist:
+                mn_dist = dist
+                ri, rj = ni, nj
+        if flg == 1:
+            grid[mi][mj] = 999
+        else:
+            grid[mi][mj] = 0
+        grid[ri][rj] = idx
+    return 0
+
+
+# bfs : return [i, j, d]
+from collections import deque
+def bfs(si, sj, ei, ej):
+    v = [[0] * n for _ in range(n)]
+    q = deque([(si, sj, 0)])
+    v[si][sj] = 1
+    d = 0
+    
+    while q:
+        si, sj, d = q.popleft()
+        if (si, sj) == (ei, ej):
+            return [si, sj, d]
+
+        for dr in [0, 1, 2, 3]:
+            ni, nj = si + dis[dr], sj + djs[dr]
+            # 안되면 grid 조건 제외하고 해보삼
+            if 0<=ni<n and 0<=nj<n and v[ni][nj] == 0: # and grid[ni][nj] <= 0 
+                v[ni][nj] = 1
+                q.append((ni, nj, d+1))
+    
+    return -1
+
+# idx번 Man 찾기
+def find_man(idx):
+    for i in range(n):
+        for j in range(n):
+            if grid[i][j] == idx:
+                return i, j
+
+# t번 편의점 찾기
+def find_cu(t):
+    for i in range(n):
+        for j in range(n):
+            if grid[i][j] == t:
+                return i, j
+
+def find_base():
+    valid_base = []
+    for i in range(n):
+        for j in range(n):
+            if grid[i][j] == -999:
+                valid_base.append((i, j))
+    return valid_base
+
+####################
+####################
+n, m = map(int, input().split())
+grid = [
+    list(map(int, input().split()))
+    for _ in range(n)
+]
+
+# 베이스 캠프 -999 로 변경
+for i in range(n):
+    for j in range(n):
+        if grid[i][j] == 1:
+            grid[i][j] = -999
+
+
+man = dict()
+# 편의점, 사람 추가
+for num in range(1, m+1):
+    i, j = map(int, input().split())
+    grid[i-1][j-1] = -num
+    # 격자 안에 들어간 정보, 도착 정보
+    man[num] = [0, 0]
+
+dis = [-1, 0, 0, 1]
+djs = [0, -1, 1, 0]
+# 격자 출력 test
+# for v in grid:
+#     print(*v)
+
+# 베이스 캠프 : -999 였다가, 사람이 발견하면 사람이 들어갔다가, 출발하면 999로 변경
+# 사람 : 1 ~ 15
+# 편의점 : -(1 ~ 30)
+
+# 도착하면 1로 변경 (베이스 캠프 속에 사람 = (-1 => 1)) -> 1 찾으러 감
+base_camp = []
+t = 1
+while 1:
+    # 종료 조건 (모두 도착)
+    end = 0
+    for idx in range(1, m+1):
+        _, e = man[idx]
+        if e == 1:
+            end += 1
+    if end == m:
+        break
+    
+    for idx in range(1, m+1):
+        # 격자 포함, 종료 여부
+        g, e = man[idx]
+        if e == 1 or g == 0:
+            continue
+        
+        # [1] : 1칸 이동 (idx번째 man이 -idx번째 편의점을 향해..)
+        if move_man(idx) == 1:
+            man[idx] = [1, 1]
+    
+    # 격자에 사람 추가
+    if t <= m:
+        # t번 편의점 찾기
+        ci, cj = find_cu(-t)
+
+        # t번 편의점과 가장 가까운 베이스캠프 찾기
+        # i, j, d (좌표, 거리) (i와 j는 !무조건! 베이스 캠프 위치)
+        can_base = []
+        base = find_base()
+        for bi, bj in base:
+            can_base.append(bfs(ci, cj, bi, bj))
+        can_base.sort(key = lambda x : (x[2], x[1], x[0]))
+        # si, sj == 선택된 베이스캠프
+        si, sj = can_base[0][0], can_base[0][1]
+        # grid에 정보 최신화 (베이스캠프 -> 사람(t))
+        base_camp.append([si, sj])
+        grid[si][sj] = t
+        man[t] = [1, 0]
+    # 1번
+    # 2번
+
+
+    t += 1
+    # for v in grid:
+    #     print(*v)
+    # if t == 4:
+    #     break
+print(t-1)
